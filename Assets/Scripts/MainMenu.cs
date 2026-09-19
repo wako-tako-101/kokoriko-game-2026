8b4ca118e2861401578589b42dd6f877241bc49c
Warrick Brugal <warry.extraz@gmail.com>
2026-09-19 07:09:15 -0400

    Build Stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(2);
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
